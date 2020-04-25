afba3c436a885c327eaa885137f11f05f4b5e7cd
Clewerton Roque <clewerton203@gmail.com>
2020-04-24 21:15:41 -0300

    Interface Finished

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Platform, StyleSheet, Text, View, Image } from 'react-native';
+import { Platform, StyleSheet, Text, View, Image, TouchableOpacity  } from 'react-native';
 
 
 
@@ -11,10 +11,13 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={{justifyContent: 'center', alignItems: 'center',}}>
+        <TouchableOpacity style={[styles.button, styles.buttonLeft]}><Text style={styles.buttonText}>+</Text></TouchableOpacity>
         <Image style={{width: 300, height: 300, position: 'absolute', zIndex: 3}} tintColor={'rgba(255,255,0,0.5)'} source = {require('./src/images/DualSenseRight.png')} />
         <Image style={{width: 300, height: 300, position: 'absolute', zIndex: 2}}  source = {require('./src/images/DualSenseRight.png')} />
+        <TouchableOpacity style={[styles.button, styles.buttonRight]}><Text style={styles.buttonText}>+</Text></TouchableOpacity>
         <Image style={{width: 300, height: 300, position: 'absolute', zIndex: 3}} tintColor={'rgba(255,0,255,0.5)'} source = {require('./src/images/DualSenseLeft.png')} />
         <Image style={{width: 300, height: 300, position: 'absolute'}} source = {require('./src/images/DualSenseLeft.png')} />
+        <TouchableOpacity style={[styles.button, styles.buttonCenter]}><Text style={styles.buttonText}>+</Text></TouchableOpacity>
         <Image style={{width: 300, height: 300, position: 'absolute', zIndex: 3}} tintColor={'rgba(0,255,255,0.5)'} source = {require('./src/images/DualSenseCenter.png')} />
         <Image style={{width: 300, height: 300, position: 'absolute'}} source = {require('./src/images/DualSenseCenter.png')} />
         <Image style={{width: 300, height: 300, position: 'absolute'}} source = {require('./src/images/background.png')} />
@@ -32,14 +35,32 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5FCFF',
     
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  button:{
+    borderWidth: 10,
+    borderColor: 'rgba(255,255,255, 0.5)',
+    width: 50,
+    height: 50,
+    borderRadius: 30,
+    position: 'absolute', 
+    zIndex: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  buttonLeft:{
+    right: 105,
+    backgroundColor: 'rgba(255, 0, 149,0.8)'
+    
   },
+  buttonRight:{
+    left: 105,
+    backgroundColor: 'rgba(255, 0, 149,0.8)'
+  },
+  buttonCenter:{
+    bottom: 60,
+    backgroundColor: 'rgba(255, 0, 149,0.8)'
+  },
+  buttonText:{
+    color: 'white'
+
+  }
 });
