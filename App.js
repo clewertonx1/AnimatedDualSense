2b2ab92e63f449ec2d58dd0cb58bcf98d2eb3eec
Clewerton Roque <clewerton203@gmail.com>
2020-04-25 16:40:33 -0300

    Project finished

--- App.js
+++ App.js
@@ -5,38 +5,37 @@ import Button from './src/components/button'
 
 export default function App() {
 
+  const [DualSenseRightColor, setDualSenseRightColor] = useState("rgba(255, 255 255, 0)")
+  const [DualSenseLeftColor, setDualSenseLeftColor] = useState("rgba(255, 255, 255, 0)")
+  const [DualSenseCenterColor, setDualSenseCenterColor] = useState("rgba(255, 255, 255, 0)")
 
-  const teste = () => {
-    console.log("teste")
+  const pinkyRgba = "rgba(255, 0, 242, 0.5)"
+  const yellowRgba = "rgba(238, 255, 0, 0.5)"
+  const darkBlueRgba = "rgba(0, 29, 255, 0.5)"
+  const blueRgba = "rgba(0, 255, 255, 0.5)"
+  const redRgba = "rgba(255, 0, 0, 0.5))"
+  const orangeRgba = "rgba(255, 110, 0, 0.5)"
+  const whiteRgba = "rgba(250, 250, 250, 0.5)"
+  const greenRgba = "rgba(0, 255, 17, 0.5)"
+
+  const changeColor = (side, color) => {
+    if(side==="Left"){
+      setDualSenseLeftColor(color)
+    }
+    if(side==="Right"){
+      setDualSenseRightColor(color)
+    }
+    if(side==="Center"){
+      setDualSenseCenterColor(color)
+    }
   }
 
-  const testeRef = useRef(teste())
-  
-  const animatedValueTop = new Animated.Value(0)
-  const animatedValueTopRef = useRef(animatedValueTop)
-  
-  Animated.loop(
-    Animated.spring(animatedValueTop, {
-      toValue: 1,
-      friction: 10,
-    }),
-    Animated.spring(animatedValueTop, {
-      toValue: 0,
-      friction: 10,
-    }),
+   
 
-  )
-     
-  
-  const translateX = animatedValueTopRef.current.interpolate({
-    inputRange: [ 0, 1],
-    outputRange: [0, 30],
-  })
-  
 
   
  // #ed349c ROSA
- // #FFF09D AMARELO
+ // #eeff00 AMARELO
  // #8F9ED7 AZUL ESCURO
  // #00ffff AZUL BB
  // #9b9b9b CINZA
@@ -46,24 +45,20 @@ export default function App() {
 
   return (
     <View style={styles.container}>
+        
+      <Image style={{width: 300, height: 300, position: 'absolute', zIndex: 1}} tintColor={DualSenseRightColor} source = {require('./src/images/DualSenseRight.png')} />
+      <Image style={{width: 300, height: 300, position: 'absolute'}}  source = {require('./src/images/DualSenseRight.png')} />
+      <Image style={{width: 300, height: 300, position: 'absolute', zIndex: 1}} tintColor={DualSenseLeftColor} source = {require('./src/images/DualSenseLeft.png')} />
+      <Image style={{width: 300, height: 300, position: 'absolute'}} source = {require('./src/images/DualSenseLeft.png')} />
+      <Image style={{width: 300, height: 300, position: 'absolute', zIndex: 1}} tintColor={DualSenseCenterColor} source = {require('./src/images/DualSenseCenter.png')} />
+      <Image style={{width: 300, height: 300, position: 'absolute'}} source = {require('./src/images/DualSenseCenter.png')} />
+
+      <Image style={{width: 300, height: 300, position: 'absolute'}} source = {require('./src/images/background.png')} />
       
 
-        
-        
-      
-        
-        <Image style={{width: 300, height: 300, position: 'absolute', zIndex: 1}} tintColor={'rgba(255,255,0,0.5)'} source = {require('./src/images/DualSenseRight.png')} />
-        <Image style={{width: 300, height: 300, position: 'absolute'}}  source = {require('./src/images/DualSenseRight.png')} />
-        <Image style={{width: 300, height: 300, position: 'absolute', zIndex: 1}} tintColor={'rgba(255,0,255,0.5)'} source = {require('./src/images/DualSenseLeft.png')} />
-        <Image style={{width: 300, height: 300, position: 'absolute'}} source = {require('./src/images/DualSenseLeft.png')} />
-        <Image style={{width: 300, height: 300, position: 'absolute', zIndex: 1}} tintColor={'rgba(0,255,255,0.5)'} source = {require('./src/images/DualSenseCenter.png')} />
-        <Image style={{width: 300, height: 300, position: 'absolute'}} source = {require('./src/images/DualSenseCenter.png')} />
-        <Image style={{width: 300, height: 300, position: 'absolute'}} source = {require('./src/images/background.png')} />
-      
-
-      <Button teste={teste} right={130}></Button>
-      <Button right={-130}></Button>
-      <Button bottom={90}></Button>
+      <Button side={"Left"} changeColor={changeColor} right={130}></Button>
+      <Button side={"Right"} changeColor={changeColor} right={-130}></Button>
+      <Button side={"Center"} changeColor={changeColor} bottom={90}></Button>
     </View>
   );
 }
--- src/components/button.js
+++ src/components/button.js
@@ -6,67 +6,79 @@ import { Platform, StyleSheet, Text, View, Image, TouchableOpacity, Animated   }
 export default function Button(params) {
 
     const [visible, setVisible] = useState(false)
-    const AnimatedTouchable = Animated.createAnimatedComponent(TouchableOpacity)
- // #ed349c ROSA
- // #FFF09D AMARELO
- // #8F9ED7 AZUL ESCURO
- // #00ffff AZUL BB
- // #9b9b9b CINZA
- // #ff6e00 LARANJA
- // #FAFAFA WHITE 
- // #00ff11 VERDE
 
-    const animatedValue = new Animated.Value(0)
-    const animatedValueRef = useRef(animatedValue)
-  
-    const color = animatedValueRef.current.interpolate({
-      inputRange: [ 0, 1],
-      outputRange: ["rgba(255,255,255, 0.3)", "rgba(255,255,255, 1)"],
-    })
-    
-    async function buttonOpen(){
-    
-        setVisible(!visible)  
+    // #ed349c ROSA
+    // #FFF09D AMARELO
+    // #8F9ED7 AZUL ESCURO
+    // #00ffff AZUL BB
+    // #9b9b9b CINZA
+    // #ff6e00 LARANJA
+    // #FAFAFA WHITE 
+    // #00ff11 VERDE
+
+    const pinkyOpacity = "rgba(255, 0, 242, 0.5)"
+    const yellowOpacity = "rgba(238, 255, 0, 0.5)"
+    const darkBlueOpacity = "rgba(0, 29, 255, 0.5)"
+    const blueOpacity = "rgba(0, 255, 255, 0.5)"
+    const redOpacity = "rgba(255, 0, 0, 0.5))"
+    const orangeOpacity = "rgba(255, 110, 0, 0.5)"
+    const whiteOpacity = "rgba(250, 250, 250, 0.5)"
+    const greenOpacity = "rgba(0, 255, 17, 0.5)"
+
+    const pinky = "rgba(255, 0, 242, 1)"
+    const yellow = "rgba(238, 255, 0, 1)"
+    const darkBlue = "rgba(0, 29, 255, 1)"
+    const blue = "rgba(0, 255, 255, 1)"
+    const red = "rgba(255, 0, 0, 1))"
+    const orange = "rgba(255, 110, 0, 1)"
+    const white = "rgba(250, 250, 250, 1)"
+    const green = "rgba(0, 255, 17, 1)"
+
+    //params.changeColor(params.side, pinkyRgba)
+
+    function handleChangeColor(color){
+        params.changeColor(params.side, color)
+        setVisible(!visible)
     }
 
     return (
    
         <Animated.View style={[styles.buttonView, { right: params.right, bottom: params.bottom}]}>
 
-            <AnimatedTouchable onPress={() => buttonOpen()} style={[styles.button, styles.buttonLeft, {borderColor: color }]}>
+            <TouchableOpacity onPress={() => setVisible(!visible)} style={[styles.button, styles.buttonLeft, ]}>
             <Text style={styles.buttonText}>+</Text>
-            </AnimatedTouchable>
+            </TouchableOpacity>
 
             {visible ? 
-                <TouchableOpacity onPress={() => toggleMenu()} style={[styles.ColorButton, {zIndex: 10, top: 23, backgroundColor: '#ed349c'}]}>
+                <TouchableOpacity onPress={() => handleChangeColor(whiteOpacity)} style={[styles.ColorButton, {zIndex: 10, top: 23, backgroundColor: white}]}>
                 </TouchableOpacity>
             : null}
             {visible ? 
-                <TouchableOpacity onPress={() => toggleMenu()} style={[styles.ColorButton, {zIndex: 10, bottom: 23, backgroundColor: '#ffd91c'}]}>
+                <TouchableOpacity onPress={() => handleChangeColor(greenOpacity)} style={[styles.ColorButton, {zIndex: 10, bottom: 23, backgroundColor: green}]}>
                 </TouchableOpacity>
             : null}
             {visible ? 
-                <TouchableOpacity onPress={() => toggleMenu()} style={[styles.ColorButton, {zIndex: 10, right: 23, backgroundColor: '#ff1e1e'}]}>
+                <TouchableOpacity onPress={() => handleChangeColor(orangeOpacity)} style={[styles.ColorButton, {zIndex: 10, right: 23, backgroundColor: orange}]}>
                 </TouchableOpacity>
             : null}
             {visible ? 
-                <TouchableOpacity onPress={() => toggleMenu()} style={[styles.ColorButton, {zIndex: 10, left: 23, backgroundColor: '#00ffff'}]}>
+                <TouchableOpacity onPress={() => handleChangeColor(redOpacity)} style={[styles.ColorButton, {zIndex: 10, left: 23, backgroundColor: red}]}>
                 </TouchableOpacity>
             : null}
             {visible ? 
-                <TouchableOpacity onPress={() => toggleMenu()} style={[styles.ColorButton, {zIndex: 10, left: 13, top: 14, backgroundColor: '#9b9b9b'}]}>
+                <TouchableOpacity onPress={() => handleChangeColor(blueOpacity)} style={[styles.ColorButton, {zIndex: 10, left: 13, top: 14, backgroundColor: blue}]}>
                 </TouchableOpacity>
             : null}
             {visible ? 
-                <TouchableOpacity onPress={() => toggleMenu()} style={[styles.ColorButton, {zIndex: 10, right: 13, bottom: 14, backgroundColor: '#8b1eff'}]}>
+                <TouchableOpacity onPress={() => handleChangeColor(darkBlueOpacity)} style={[styles.ColorButton, {zIndex: 10, right: 13, bottom: 14, backgroundColor: darkBlue}]}>
                 </TouchableOpacity>
             : null}
             {visible ? 
-                <TouchableOpacity onPress={() => toggleMenu()} style={[styles.ColorButton, {zIndex: 10, left: 13, bottom: 14, backgroundColor: '#FAFAFA'}]}>
+                <TouchableOpacity onPress={() => handleChangeColor(yellowOpacity)} style={[styles.ColorButton, {zIndex: 10, left: 13, bottom: 14, backgroundColor: yellow}]}>
                 </TouchableOpacity>
             : null}
             {visible ? 
-                <TouchableOpacity onPress={() => toggleMenu()} style={[styles.ColorButton, {zIndex: 10, right: 13, top: 14, backgroundColor: '#00ff11'}]}>
+                <TouchableOpacity onPress={() => handleChangeColor(pinkyOpacity)} style={[styles.ColorButton, {zIndex: 10, right: 13, top: 14, backgroundColor: pinky}]}>
                 </TouchableOpacity> 
             : null}
                    
